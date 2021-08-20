007cffa96851a86e997de27f86192c66377de208
rok <roky.mlakar@gmail.com>
2021-08-20 15:18:24 +0200

    Create api response models and init the HttpClient instance

---
+++ src/ExpenseTracker.Shared/Respones/ApiErrorResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ExpenseTracker.Shared.Respones
+{
+    public class ApiErrorResponse
+    {
+        public string Messsage { get; set; }
+        public string[] Errors { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+
+}
--- src/ExpenseTracker/Program.cs
+++ src/ExpenseTracker/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,10 +19,15 @@ namespace ExpenseTracker
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient("ExpenseTracker.Api", client =>
+            {
+                client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net");
+            }).AddHttpMessageHandler<AuthorizationMessageHandler>();
+            builder.Services.AddTransient<AuthorizationMessageHandler>();
+            builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("ExpenseTracker.Api"));
 
             builder.Services.AddMudServices();
-
+            builder.Services.AddBlazoredLocalStorage();
             await builder.Build().RunAsync();
         }
     }
