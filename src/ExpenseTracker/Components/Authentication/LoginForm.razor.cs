fb48201bdb5bc6219e2a3bcaffd10664de7676e7
rok <roky.mlakar@gmail.com>
2021-08-29 17:47:24 +0200

    Create the auth layout & login form

---
+++ src/ExpenseTracker/Components/Authentication/LoginForm.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ExpenseTracker.Shared.Models;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Components
+{
+    public partial class LoginForm : ComponentBase 
+    {
+
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        private LoginRequest _model = new LoginRequest();
+
+        private async Task LoginUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
